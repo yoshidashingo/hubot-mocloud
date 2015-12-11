bd38a04fef91106fc38e58f58193946d656e510c
yoshidashingo <yoshida.shingo.com@gmail.com>
2015-12-11 20:43:52 +0900

    initial commit

---
+++ mobingi-install.sh
@@ -0,0 +1,7 @@
+npm install -g coffee-script hubot hubot-slack
+
+cp -r /var/www/html /root/scripts 
+
+echo "[program:hubot]" >> /etc/supervisor/conf.d/supervisord.conf
+echo "command=hubot --adapter slack" >> /etc/supervisor/conf.d/supervisord.conf
+echo "directory=/root" >> /etc/supervisor/conf.d/supervisord.conf
