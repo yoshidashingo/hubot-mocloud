c78b1796deb97dc48f2f0e633c76d492b36d54e4
yoshidashingo <yoshida.shingo.com@gmail.com>
2015-12-14 17:24:21 +0900

    fix hubot command

--- my-install.sh
+++ my-install.sh
@@ -4,5 +4,5 @@ npm -y install -g coffee-script hubot hubot-slack
 cp -r /var/www/html /root/scripts 
 
 echo "[program:hubot]" >> /etc/supervisor/conf.d/supervisord.conf
-echo "command=hubot --adapter slack" >> /etc/supervisor/conf.d/supervisord.conf
+echo "command=/usr/local/bin/hubot --adapter slack" >> /etc/supervisor/conf.d/supervisord.conf
 echo "directory=/root" >> /etc/supervisor/conf.d/supervisord.conf
