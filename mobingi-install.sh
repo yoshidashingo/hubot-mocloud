6744b9c37f086f4e3dac6fba1bb9686b42fafb12
yoshidashingo <yoshida.shingo.com@gmail.com>
2015-12-15 09:10:41 +0900

    Fix install

--- mobingi-install.sh
+++ mobingi-install.sh
@@ -1,10 +1,14 @@
 apt-get -y install nodejs npm
-npm -y install -g coffee-script hubot hubot-slack
-
 update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
+npm -y install -g hubot coffee-script yo generator-hubot
+
+#cd /root/scripts
+#yo hubot
+
+npm -y install hubot-slack --save
 
 cp -r /var/www/html /root/scripts
 
 echo "[program:hubot]" >> /etc/supervisor/conf.d/supervisord.conf
-echo "command=/usr/local/bin/hubot --adapter slack" >> /etc/supervisor/conf.d/supervisord.conf
+echo "command=hubot --adapter slack" >> /etc/supervisor/conf.d/supervisord.conf
 echo "directory=/root" >> /etc/supervisor/conf.d/supervisord.conf
