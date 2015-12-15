f8825aa03e54f348089c9a042420633fdb94fa24
yoshidashingo <yoshida.shingo.com@gmail.com>
2015-12-15 09:17:29 +0900

    Fix install

--- mobingi-install.sh
+++ mobingi-install.sh
@@ -1,11 +1,11 @@
 apt-get -y install nodejs npm
 update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
-npm -y install -g hubot coffee-script yo generator-hubot
+#npm -y install -g hubot coffee-script yo generator-hubot
+npm -y install -g hubot coffee-script hubot-slack
 
-#cd /root/scripts
-#yo hubot
-
-npm -y install hubot-slack --save
+# cd /root/scripts
+# yo hubot
+# npm -y install hubot-slack --save
 
 cp -r /var/www/html /root/scripts
 
